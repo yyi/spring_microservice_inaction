d88771689943dd4b7b3eea62509e8961ec1ebe5e
yyi.mailer@gmail.com <yangyigood108>
2018-09-18 10:16:02 +0800

    update

--- spmia-chapter3-master/licensing-service/src/main/java/com/thoughtmechanix/licenses/config/ServiceConfig.java
+++ spmia-chapter3-master/licensing-service/src/main/java/com/thoughtmechanix/licenses/config/ServiceConfig.java
@@ -1,9 +1,11 @@
 package com.thoughtmechanix.licenses.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Component;
 
 @Component
+@RefreshScope
 public class ServiceConfig{
 
   @Value("${example.property}")
